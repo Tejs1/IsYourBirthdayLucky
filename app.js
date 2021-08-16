8a7cbd37cbb5a56e5947ae8ddcfddd63d0bd1728
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-16 14:12:17 +0530

    getting inputs in js

---
+++ app.js
@@ -0,0 +1,4 @@
+const date = document.getElementById("date");
+const button = document.getElementById("submit");
+const lNumber = document.getElementById("number");
+const output = document.getElementById("output");
