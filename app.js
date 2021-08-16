558f655a291e3288142a74ae081e7e119355400a
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-16 14:38:32 +0530

    process aded

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const button = document.getElementById("submit");
 const lNumber = document.getElementById("number");
 const output = document.getElementById("output");
 
-
 //Submit btn click handler for checking lucky birthdate
 function clickHandler() {
-    const userDate = date.value.split("-");
-  
-    const year = parseInt(userDate[0]); //year
-  
-    const month = parseInt(userDate[1]); //month
-  
-    const day = parseInt(userDate[2]); //day
+  const userDate = date.value.split("-");
+  // console.log(userDate);
+  const year = parseInt(userDate[0]); //year
+  // console.log(userDate[0]);
+  const month = parseInt(userDate[1]); //month
+  // console.log(userDate[1]);
+  const day = parseInt(userDate[2]); //day
+  // console.log(userDate[2]);
 
-//finding sum of digits of birthday is divisible by their lucky number
+  //finding sum of digits of birthday is divisible by their lucky number
   const luck = year + month + day;
   const number = lNumber.value;
+
+  if (luck % number === 0 || luck % number === 5) {
+    output.innerHTML = `<p class="img-para">YEAH! Your Birthdate is Lucky XD</p>`;
+  } else {
+    output.innerHTML = `
+    <p class="img-para">OOPS! Your Birthdate is not Lucky ;(</p>`;
+  }
+}
+
+button.addEventListener("click", clickHandler);
