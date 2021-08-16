94562eea2c33636c6811a2694b0103266c216c9c
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-16 14:14:08 +0530

    Function to add digits of date

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const date = document.getElementById("date");
 const button = document.getElementById("submit");
 const lNumber = document.getElementById("number");
 const output = document.getElementById("output");
+
+
+//Submit btn click handler for checking lucky birthdate
+function clickHandler() {
+    const userDate = date.value.split("-");
+  
+    const year = parseInt(userDate[0]); //year
+  
+    const month = parseInt(userDate[1]); //month
+  
+    const day = parseInt(userDate[2]); //day
+
+//finding sum of digits of birthday is divisible by their lucky number
+  const luck = year + month + day;
+  const number = lNumber.value;
